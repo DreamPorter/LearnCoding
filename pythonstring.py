9d1b5e90e2984a9da8a1b960a42cda022e6edaab
Dreamporter <u490498@gmail.com>
2019-10-13 17:32:56 -0700

    pythonstring

---
+++ pythonstring.py
@@ -0,0 +1,35 @@
+
+s = 'spam'
+
+#sequence operations
+
+'''
+print(len(s))
+
+print(s[0])
+print(s[1])
+
+print(s[-1])
+print(s[-2])
+
+#slicing
+print(s[:])  #浅拷贝
+print(s[1:]) # pam
+print(:-1) #spa
+
+s += 'xyz'
+print(s)
+print(s * 8)
+
+'''
+#immutability
+
+'''
+s[0] = 'x' # error 
+
+s = 'z' + s[:]
+print(s) #zspam
+
+'''
+
+
